7365459a5a18fc8dcd93182ad07c8442ebe92513
pooya98 <pooya98@naver.com>
2020-12-05 18:27:23 +0900

    (로그인, QR코드 스캔, 출입정보 등록, 출입정보 조회 기능 구현)

---
+++ app/src/main/java/com/example/corona_tracker/DAO.java
@@ -0,0 +1,525 @@
+package com.example.corona_tracker;
+
+import android.view.View;
+
+import com.example.corona_tracker.AdminClass.NotiData;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DAO {
+    boolean admin_account_flag;
+    boolean building_id_flag;
+    String building_name;
+    boolean insert_enter_info_flag;
+    boolean insert_notice_flag;
+    boolean delete_notice_flag;
+    boolean update_notice_flag;
+
+    HttpClient httpclient;
+    HttpPost httppost;
+    HttpResponse response;
+    java.util.List<NameValuePair> nameValuePairs;
+    ArrayList<NotiData> list = null;
+    NotiData notiData = null;
+
+    public ArrayList<NotiData> get_notiData_array(){
+
+        list = null;
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                get_notice();
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+        return list;
+    }
+
+    public NotiData get_notiData(final int notice_id){
+
+        notiData = null;
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                get_notiData_con(notice_id);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+        return notiData;
+    }
+
+
+    public boolean insert_notice(final NotiData insert_notiData){
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                insert_notice_con(insert_notiData);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return insert_notice_flag;
+    }
+
+    public boolean update_notice(final NotiData update_notiData){
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                update_notice_con(update_notiData);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return update_notice_flag;
+    }
+
+    public boolean check_admin_account(final String admin_ID, final String admin_PW){
+
+        boolean success_flag = false;
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                check_admin_account_con(admin_ID, admin_PW);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return admin_account_flag;
+    }
+
+    public boolean check_building_id(final String building_id){
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                check_building_id_con(building_id);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return building_id_flag;
+    }
+
+    public String get_building_name(final String building_id){
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                get_building_name_con(building_id);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return building_name;
+    }
+
+    public boolean insert_enter_info(final String building_id, final String date, final String time, final String student_id, final String student_phone){
+
+        Thread loadThread = new Thread(){
+            public void run(){
+                insert_enter_info_con(building_id, date, time, student_id, student_phone);
+            }
+        };
+
+        loadThread.start();
+        System.out.println("--- loadThread go!");
+
+        try{
+            loadThread.join();
+            System.out.println("--- loadThread done!");
+        }catch(Exception e){
+            e.getMessage();
+        }
+
+        return insert_enter_info_flag;
+    }
+
+
+
+
+
+
+
+    //
+    //
+    //
+    //
+
+
+
+    void get_notice(){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/show_notice.php"); // php주소 연동
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+            }
+
+            System.out.println("uuuuu"+response_string);
+
+            String[] token;
+            token = response_string.split("\\$");
+
+            System.out.println("ddddd"+token.length);
+            if(token[0].equals("")){
+            }
+            else {
+                list = new ArrayList<NotiData>();
+                for (int i = 0; i < token.length; i = i+3) {
+                    NotiData notiData = new NotiData();
+
+                    //notiData.setID(token[i]);
+                    System.out.println("token[0] - "+token[i]);
+                    notiData.setTitle(token[i+1]);
+                    System.out.println("token[1] - "+token[i+1]);
+                    notiData.setTimeFromText(token[i+2]);
+                    System.out.println("token[2] - "+token[i+2]);
+                    list.add(notiData);
+                }
+            }
+
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void get_notiData_con(int notice_id){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/get_notidata.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("notice_id",Integer.toString(notice_id)));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+            }
+
+            System.out.println("uuuuu"+response_string);
+
+            String[] token;
+            token = response_string.split("\\$");
+
+            System.out.println("ddddd"+token.length);
+            if(token[0].equals("")){
+            }
+            else {
+                list = new ArrayList<NotiData>();
+                for (int i = 0; i < token.length; i = i+3) {
+                    NotiData notiData = new NotiData();
+
+                    //notiData.setID(token[i]);
+                    System.out.println("token[0] - "+token[i]);
+                    notiData.setTitle(token[i+1]);
+                    System.out.println("token[1] - "+token[i+1]);
+                    //notiData.setAuthorID(token[i+2]);
+                    System.out.println("token[2] - "+token[i+2]);
+                    notiData.setContent(token[i+3]);
+                    System.out.println("token[3] - "+token[i+3]);
+                    notiData.setTimeFromText(token[i+4]);
+                    System.out.println("token[4] - "+token[i+4]);
+                    break;
+                }
+            }
+
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void check_admin_account_con(String admin_ID, String admin_PW){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/check_admin_account.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("admin_ID",admin_ID));
+            nameValuePairs.add(new BasicNameValuePair("admin_PW",admin_PW));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    admin_account_flag = true;
+                }else{
+                    admin_account_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void check_building_id_con(String building_id){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/check_building_id.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("B_ID", building_id));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    building_id_flag = true;
+                }else{
+                    building_id_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void get_building_name_con(String building_id){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/get_building_name.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("B_ID", building_id));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                building_name = response_string;
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void insert_enter_info_con(String building_id, String date, String time, String student_id, String student_phone){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/insert_enter_info.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("building_id", building_id));
+            nameValuePairs.add(new BasicNameValuePair("date", date));
+            nameValuePairs.add(new BasicNameValuePair("time", time));
+            nameValuePairs.add(new BasicNameValuePair("student_id", student_id));
+            nameValuePairs.add(new BasicNameValuePair("student_phone", student_phone));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    insert_enter_info_flag = true;
+                }else{
+                    insert_enter_info_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void insert_notice_con(NotiData insert_notiData){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/insert_notice.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("title", insert_notiData.getTitle()));
+            //nameValuePairs.add(new BasicNameValuePair("manager_id", insert_notiData.getAuthorID()));
+            nameValuePairs.add(new BasicNameValuePair("manager_id", "manager1"));
+            nameValuePairs.add(new BasicNameValuePair("contents", insert_notiData.getContent()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    insert_notice_flag = true;
+                }else{
+                    insert_notice_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void update_notice_con(NotiData update_notiData){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/update_notice.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            //nameValuePairs.add(new BasicNameValuePair("notice_id", update_notiData.getNoticeID()));
+            nameValuePairs.add(new BasicNameValuePair("notice_id", Integer.toString(13)));
+            nameValuePairs.add(new BasicNameValuePair("new_title", update_notiData.getTitle()));
+            //nameValuePairs.add(new BasicNameValuePair("manager_id", insert_notiData.getAuthorID()));
+            nameValuePairs.add(new BasicNameValuePair("new_contents", update_notiData.getContent()));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    update_notice_flag = true;
+                }else{
+                    update_notice_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    void delete_notice_con(int notice_id){
+        try{
+
+            httpclient=new DefaultHttpClient();
+            httppost = new HttpPost("http://14.45.108.71:80/corona_tracker/delete_notice.php"); // php주소 연동
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("index", Integer.toString(notice_id)));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+            response = httpclient.execute(httppost);
+
+            Scanner scan = new Scanner(response.getEntity().getContent());
+
+            String response_string = "";
+            while(scan.hasNext()){
+                response_string += scan.nextLine();
+                if(response_string.equals("T")){
+                    delete_notice_flag = true;
+                }else{
+                    delete_notice_flag = false;
+                }
+            }
+
+            System.out.println("---Response : "+response_string);
+
+        }catch(Exception E){
+            E.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/corona_tracker/login.java
+++ app/src/main/java/com/example/corona_tracker/login.java
@@ -1,8 +1,13 @@
 package com.example.corona_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,18 +15,66 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.example.corona_tracker.AdminClass.AdminActivity;
+
 public class login extends AppCompatActivity {
     boolean islogin;
     String mode = "student";
     String studentID, adminID, password; // 학번, 관리자ID, 관리자 비밀번호
     EditText edit_studentID, edit_adminID, edit_password;
+    private boolean saveLoginData;
+    private SharedPreferences appData;
+
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        //디바이스 id 권한 허용
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+            }
+        }
+
+
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        load();
+
+        if(saveLoginData == true){
+            complete(true);
+        }
+
         islogin = false;
+        mode = "student";
         changeMode(R.id.login_student);
+    }
+
+    // 디바이스 id 권한 허용
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+                return;
+            }
+        }
     }
 
     public void setMode(View view) { // 학생(관리자)로 모드 전환 버튼 이벤트
@@ -67,12 +120,20 @@ public class login extends AppCompatActivity {
 
     public void login(View view) { // 입력된 정보로 로그인 시도
         int id = view.getId();
+        DAO dao = new DAO();
+
         switch (id){
             case R.id.confirm_student: // 학생으로 로그인
                 edit_studentID = (EditText) findViewById(R.id.editText_studentID);
                 studentID = edit_studentID.getText().toString();
                 if(studentID.length() == 0){ // 입력되어 있지 않음
                     Toast.makeText(this,"학번을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                    islogin = false;
+                    return;
+                }
+                if(studentID.length()!= 10){
+                    Toast.makeText(this,"유효하지 않은 학번입니다.\n 다시 학번을 입력해주세요.",Toast.LENGTH_SHORT).show();
+                    edit_studentID.setText("");
                     islogin = false;
                     return;
                 }
@@ -87,6 +148,13 @@ public class login extends AppCompatActivity {
                     islogin = false;
                     return;
                 }
+
+                if(!dao.check_admin_account(adminID, password)){
+                    Toast.makeText(this,"존재하지 않는 ID 이거나 Password가 틀렸습니다.",Toast.LENGTH_SHORT).show();
+                    islogin = false;
+                    edit_password.setText("");
+                    return;
+                }
                 break;
         }
 
@@ -95,7 +163,45 @@ public class login extends AppCompatActivity {
     }
 
     public void complete(boolean islogin){ // 로그인 완료 함수 ( finish )
-        if(islogin == true) finish();
+
+        if(mode.equals("student")) {
+            SharedPreferences.Editor editor = appData.edit();
+            editor.putBoolean("SAVE_LOGIN_DATA", true);
+            editor.putString("SAVE_LOGIN_MODE", "student");
+            editor.putString("studentID", studentID);
+            editor.putString("adminID","");
+            editor.putString("password","");
+
+            editor.apply();
+            Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }else{
+            SharedPreferences.Editor editor = appData.edit();
+            editor.putBoolean("SAVE_LOGIN_DATA", true);
+            editor.putString("SAVE_LOGIN_MODE", "admin");
+            editor.putString("adminID", adminID);
+            editor.putString("password",password);
+            editor.putString("studentID", "");
+
+            editor.apply();
+            Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, AdminActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
+    // 설정값을 불러오는 함수
+    private void load() {
+        // SharedPreferences 객체.get타입( 저장된 이름, 기본값 )
+        // 저장된 이름이 존재하지 않을 시 기본값
+        saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA", false);
+        mode = appData.getString("SAVE_LOGIN_MODE", "");
+        adminID = appData.getString("adminID", "");
+        password = appData.getString("password", "");
+        studentID = appData.getString("studentID","");
+    }
 }
